36929f1c73b19f7565e97089776f6e6809b1b5bf
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2017-11-22 15:30:38 +0700

    Add SplashScreen, IntroScreen, LoginScreen

--- app/src/main/java/id/noidea/firstblood/SplashActivity.java
+++ app/src/main/java/id/noidea/firstblood/SplashActivity.java
@@ -1,13 +1,30 @@
 package id.noidea.firstblood;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 3000;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
